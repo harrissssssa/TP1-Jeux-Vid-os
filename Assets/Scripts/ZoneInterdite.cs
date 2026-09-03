4a0d5575cbb437f7aa43ac17e08783ad2e63875e
Arris Haddad <2467554@cegepmv.ca>
2026-09-03 16:49:28 -0400

    Ajouter la zone interdite

--- Assets/Scripts/ZoneInterdite.cs
+++ Assets/Scripts/ZoneInterdite.cs
@@ -6,7 +6,16 @@ public class ZoneInterdite : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D autre)
     {
-        // TODO 1 : vérifier que l'objet possède le tag Player.
+        if (autre.CompareTag("Player"))
+        {
+            if (pointDepart == null)
+            {
+                Debug.LogError("Le point de départ n'est pas assigné.");
+                return;
+            }
+            autre.transform.position = pointDepart.position;
+            Debug.Log("Le robot retourne au point de départ.");
+        }
         // TODO 2 : vérifier que PointDepart est assigné.
         // TODO 3 : replacer le joueur et afficher un message.
     }
