a7d0f2d80e360ac96ee6815232dd1c377fd5a899
Arris Haddad <2467554@cegepmv.ca>
2026-09-03 16:34:03 -0400

    Ajouter la collecte et le compteur

--- Assets/Scripts/Collecteur.cs
+++ Assets/Scripts/Collecteur.cs
@@ -9,13 +9,30 @@ public class Collecteur : MonoBehaviour
 
     private void Start()
     {
-        // TODO 1 : vérifier que la porte est assignée.
-        // TODO 2 : cacher la porte au lancement du jeu.
+        if (porteSortie == null)
+        {
+            Debug.LogError("La porte de sortie n'est pas assignée.");
+            return;
+        }
+        porteSortie.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D autre)
     {
-        // TODO 3 : vérifier que l'objet touché est une batterie.
+        if (autre.CompareTag("Batterie"))
+        {
+            batteriesCollectees++;
+            Debug.Log($"Batteries : {batteriesCollectees}/{objectif}");
+            Destroy(autre.gameObject);
+        }
+            if (batteriesCollectees >= objectif)
+            {
+                porteSortie.SetActive(true);
+                Debug.Log("PORTE DÉVERROUILLÉE !");
+            return ;
+            }
+                
+        
         // TODO 4 : augmenter le compteur et afficher la progression.
         // TODO 5 : détruire uniquement la batterie touchée.
         // TODO 6 : afficher la porte lorsque l'objectif est atteint.
